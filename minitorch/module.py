4175bf14a3bb60a9092ef6bced8ac19559ad3f2a
diracdelta7 <xin.fu.7@qq.com>
2026-04-21 13:50:43 +0800

    Complete task 0.4 - 0.5

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,15 @@ class Module:
 
     def train(self) -> None:
         """Set the mode of this module and all descendent modules to `train`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for m in self.modules():
+            m.train()
 
     def eval(self) -> None:
         """Set the mode of this module and all descendent modules to `eval`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+        for m in self.modules():
+            m.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """Collect all the parameters of this module and its descendents.
@@ -47,13 +49,24 @@ class Module:
             The name and `Parameter` of each ancestor parameter.
 
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        result = []
+        for k, v in self.__dict__["_parameters"].items():
+            result.append((k, v))
+        for m_name, m in self.__dict__["_modules"].items():
+            for p_name, param in m.named_parameters():
+                result.append((m_name + "." + p_name, param))
+
+        return result
 
     def parameters(self) -> Sequence[Parameter]:
         """Enumerate over all the parameters of this module and its descendents."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        result = []
+        p: dict[str, Parameter] = self.__dict__["_parameters"]
+        for param in p.values():
+            result.append(param)
+        for m in self.modules():
+            result.extend(m.parameters())
+        return result
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """Manually add a parameter. Useful helper for scalar parameters.
---
+++ task05.md
@@ -0,0 +1,14 @@
+# Task 0.5: Visualization
+
+## Hyperparameters
+
+* Learning rate: 1.0
+* Number of epochs: 1500
+* Size of hidden layer: 64
+
+## Images
+
+![Circle](./imgs/Circle.png "Circle")
+![Circle_initial](./imgs/Circle_initial.png "Initial")
+![Circle_final](./imgs/Circle_final.png "Final")
+![Circle_loss](./imgs/Circle_loss.png "Loss")
